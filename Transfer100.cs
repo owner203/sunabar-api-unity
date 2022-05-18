453325c011a4a6c275efa72a7b943d7b02fb5843
owner203 <38290825+owner203@users.noreply.github.com>
2022-05-18 10:51:20 +0900

    Add files via upload

---
+++ Transfer100.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transfer100 : MonoBehaviour
+{
+    public void Transfer100Click()
+    {
+        SunabarApiAction.DoTransfer("100");
+    }
+}
